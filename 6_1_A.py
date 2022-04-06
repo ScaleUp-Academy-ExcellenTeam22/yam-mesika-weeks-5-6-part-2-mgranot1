289dc112cb035b3f24a9c3daa5fd77d7ad657a3a
mgranot1 <80787986+mgranot1@users.noreply.github.com>
2022-04-06 18:13:53 +0300

    6_1_A Fast and reliable

---
+++ 6_1_A.py
@@ -0,0 +1,35 @@
+import statistics
+from timeit import default_timer as timer
+#filename = r"C:\Networks\5-6\words.txt"
+
+
+def average_runtime(words):
+    """
+    :param words: List or Set of words
+    :return: Average time between 1000 word 'zwitterion' searches
+    """
+    list_of_search_times = []
+    for i in range(1000):
+        start_search = timer()
+        'zwitterion' in words
+        end_search = timer()
+        list_of_search_times.append(end_search - start_search)
+    return statistics.mean(list_of_search_times)
+
+
+def main():
+    file_path = input("Enter path of words.txt: ")
+    # Reading all the words from the file
+    with open(file_path) as file:
+        words_list = file.readlines()
+    # Average time of search within a list
+    words_set = set(words_list)
+    # Average time of search within a set
+    average_runtime_list = average_runtime(words_list)
+    average_runtime_set = average_runtime(words_set)
+    print("Average time to search for a word in a list: ", average_runtime_list)
+    print("Average time to search for a word in a set: ", average_runtime_set)
+
+
+if __name__ == "__main__":
+    main()
